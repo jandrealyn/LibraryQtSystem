c86a686ebaa2b9191d70d92709f520db2b5a585f
Jakob <jakobfredd99@gmail.com>
2021-11-22 13:37:32 +1300

    Revert "Merge branch 'jakob' into dev"
    This reverts commit 29bccbc8497a74dd4023896e8638d4ec797d5200.

--- NewWindowChur/reservebook.cpp
+++ NewWindowChur/reservebook.cpp
@@ -1,21 +1,3 @@
-// ------------------------------------------------------------
-//
-// RESERVE BOOK
-// Written by Jakob
-//
-// The Reserve book screen is only accessible once a book has run
-// out of available copies. This allows a user to book for the book
-// in advance so that they may have first dibs once a book has been
-// returned.
-//
-// There is a set minimum and maximum date.
-// The minimum date is found through a SystemFiles function, the
-// maximum date is the minimum date + 14.
-//
-// ------------------------------------------------------------
-
-
-
 #include "reservebook.h"
 #include "ui_reservebook.h"
 #include "SystemFiles.h"
@@ -34,12 +16,8 @@ ReserveBook::ReserveBook(QWidget *parent, QString memName, QString memID, QStrin
     _bookID = bookID;
     _bookName = bookName;
 
-    // FindLastReserveDate finds the earliest that a user is able to reserve a book.
     QDate minimumDate = SystemFiles::FindLastReserveDate(_bookID);
-
-    // Setting the minimum and maximum date
     ui->calendarWidget->setMinimumDate(minimumDate);
-    ui->calendarWidget->setMaximumDate(minimumDate.addDays(14));
     ui->bookNameLabel->setText(_bookName);
     ui->returnDateLabel->setText(minimumDate.addDays(7).toString("dd/MM/yyyy"));
 }
@@ -59,7 +37,6 @@ void ReserveBook::on_cancel_clicked()
 void ReserveBook::on_calendarWidget_clicked(const QDate &date)
 {
     ui->returnDateLabel->setText(date.addDays(7).toString("dd/MM/yyyy"));
-    // highlight dates somehow
 }
 
 
